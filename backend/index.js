1e5650b52fc900bd88ef2bb47fc9232a268211e4
@maegmail <mae003@gmail.com>
2024-09-04 02:40:25 -0500

    cambios realizados

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+  res.sendFile(path.join(__dirname, 'descriptivos.html'));
 });
 
 app.listen(PORT, () => {
